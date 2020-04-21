22697def7138edf0ec48207811959c52249862fb
Adelhide666 <61150136+Adelhide666@users.noreply.github.com>
2020-04-21 21:29:18 +0300

    Add files via upload

---
+++ JS Variables/Task 2/main.js
@@ -0,0 +1,27 @@
+function calculateResult() {
+    let firstValue = +document.querySelector("#first").value;
+    let secondValue = +document.querySelector("#second").value;
+
+    alert(firstValue + secondValue);
+}
+
+function calculateResultminus() {
+    let firstValue = +document.querySelector("#first").value;
+    let secondValue = +document.querySelector("#second").value;
+
+    alert(firstValue - secondValue);
+}
+
+function calculateResultumn() {
+    let firstValue = +document.querySelector("#first").value;
+    let secondValue = +document.querySelector("#second").value;
+
+    alert(firstValue * secondValue);
+}
+
+function calculateResultdel() {
+    let firstValue = +document.querySelector("#first").value;
+    let secondValue = +document.querySelector("#second").value;
+
+    alert(firstValue / secondValue);
+}
